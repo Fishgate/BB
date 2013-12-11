4f11e3d8f3d7c32841a6383b6cfb79c1c4ecb2fe
Tyrone <ty@crayoncapsule.co.za>
2013-12-11 09:43:31 +0200

    Tyrone gets the terms modal to work using foundations revealJS library

--- win_a_vintage_bicycle.php
+++ win_a_vintage_bicycle.php
@@ -17,6 +17,11 @@
         <link rel="stylesheet" href="css/reveal.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
         
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
+        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/jquery.reveal.js"></script>
+        
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -50,8 +55,26 @@
             <!--  -->
             <div class="sections2 left clearfix">
                 <div id="myModal" class="reveal-modal">
-                    <h1>Modal Title</h1>
-                    <p>Any content could go in here.</p>
+                    <h1>BROTHERS BEVERAGES – WIN A SCHWINN S1 CRUISER WITH ROOIBOS FRUTEA</h1>
+                    <ol>
+                        <li>WIN A SCHWINN S1 CRUISER WITH ROOIBOS FRUTEA<br />competition is organised by Brothers Beverages, owners of the brand Rooibos Frutea, and is open to South African and Namibian residents, except directors and employees of Brothers Beverages and their families, advertising agency staff or consultants of Brothers Beverages and the immediate family members of aforesaid persons (including, but not limited to, spouses, life partners, parents, children, brothers and sisters).</li>
+                        <li>These rules may be amended by notification at any time during the Competition, and will be interpreted by Promoter only. Participation by all entrants ("Participants") constitutes acceptance of these rules.</li>
+                        <li>WIN A SCHWINN S1 CRUISER WITH ROOIBOS FRUTEA competition commences on 1 December 2013 and closes on 4 May 2014.</li>
+                        <li>To qualify for the prize the consumer must enter the competition on <a href="http://www.rooibosfrutea.co.za" target="_blank">www.rooibosfrutea.co.za</a></li>
+                        <li>Each cellphone number may only be entered once.</li>
+                        <li>The winner will be selected from all valid entries received.</li>
+                        <li>The prize draw will take place a week after the competition closes and the winner will be notified by telephone and announced on the website.</li>
+                        <li>If a winner is indebted to Brothers Beverages in any way, he or she will be disqualified from receiving the prize.</li>
+                        <li>By entering WIN A SCHWINN S1 CRUISER WITH ROOIBOS FRUTEA all entrants agree to be bound by the terms and conditions of the competition, which will be interpreted by Brothers Beverages.</li>
+                        <li>The decision by the competition judges regarding all matters relating to the competition, including any dispute, is final and binding and no correspondence will be entered into.</li>
+                        <li>If a person is under 18 they must be duly assisted by a parent or legal guardian in entering the competition and in using the prize.</li>
+                        <li>The prize is not transferable or redeemable for cash or other alternatives and may not be given, assigned or sold to another person.</li>
+                        <li>Failure to claim a Prize in the time or manner specified for the relevant Competition will make any claim invalid.</li>
+                        <li>The winner will be required to sign a consent that he or she will allow Brothers Beverages or their representatives, to photograph him or her for promotional purposes related to the WIN A SCHWINN S1 CRUISER WITH ROOIBOS FRUTEA competition without payment or additional compensation.</li>
+                        <li>If in the event that the competition is terminated, whether by the DTI, or and other entity, all entrants will waive any rights which they may have against Brothers Beverages.</li>
+                        <li>Winner shall be required to indemnify Brothers Beverages for any damage or loss they may suffer from winning the prize.</li>
+                        <li>These Terms and Conditions are available on the <a href="http://www.rooibosfrutea.co.za" target="_blank">www.rooibosfrutea.co.za</a> website </li>
+                    </ol>
                     <a class="close-reveal-modal">&#215;</a>
                </div>
                 <div class='formholder'>
@@ -63,17 +86,15 @@
                         <input type='text' id='feature' value='Name one unique feature of Rooibos Frutea' />
                         <input type='submit' id='submit_btn' value='SUBMIT' />
                     </form>
-                    <p class='terms'>Please read the <a href="#" class="big-link" data-reveal-id="myModal">terms and conditions</a> for this competition</p>
+                    <p class='terms'>Please read the <a href="#" data-reveal-id="myModal">terms and conditions</a> for this competition</p>
+                    	
                 </div>
             </div>
         </div>
         
         <?php include 'includes/footer.php' ?>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         
-        <script src="js/jquery.reveal.js"></script>
         
         <script src="js/plugins.js"></script>
         <script src="js/jquery.slides.js"></script>
